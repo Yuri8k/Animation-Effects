55b8f8602e16d29eb626277cfb3f854b946961a5
unknown <kamuiakira.yo@gmail.com>
2022-10-21 00:27:36 -0300

    add rocket and emoji efects

--- js/main/header.js
+++
@@ -1,7 +0,0 @@
-const mainHeader = document.querySelector('.main-header');
-const header = document.querySelector('header');
-
-if(mainHeader && header) {
-    const headerHeight = header.getBoundingClientRect().height;
-    mainHeader.style.paddingTop = headerHeight + 'px';
-}
